74021bc9ce3d3a67f08f0e6b761aa22a9fd2dedc
Davirain <davirain.yin@gmail.com>
2023-06-28 11:17:45 +0800

    Add BassAccount and ModuleAccount

--- crates/modules/auth/src/context.rs
+++ crates/modules/auth/src/context.rs
@@ -1,3 +1,27 @@
+/// AccountI is an interface used to store coins at a given address within state.
+/// It presumes a notion of sequence numbers for replay protection,
+/// a notion of account numbers for replay protection for previously pruned accounts,
+/// and a pubkey for authentication purposes.
+///
+/// Many complex conditions can be used in the concrete struct which implements AccountI.
+pub trait AccountI: std::fmt::Display {
+    type Address;
+    type PubKey;
+    type Error;
+
+    fn get_address(&self) -> &Self::Address;
+    fn set_address(&mut self, address: Self::Address) -> Result<(), Self::Error>;
+
+    fn get_pub_key(&self) -> &Self::PubKey;
+    fn set_pub_key(&mut self, pubkey: Self::PubKey) -> Result<(), Self::Error>;
+
+    fn get_account_number(&self) -> u64;
+    fn set_account_number(&mut self, account_number: u64) -> Result<(), Self::Error>;
+
+    fn get_sequence(&self) -> u64;
+    fn set_sequence(&mut self, sequence: u64) -> Result<(), Self::Error>;
+}
+
 pub trait Account {
     /// Account address type
     type Address;
